e61fdcb267fd0c90c9b6fc9917682d6420d7a8ab
Ali Hasan <ali.hasan@northsouth.edu>
2021-12-19 09:29:46 +0600

    react router issue solved

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import ListItemComponent from './Components/ListItemComponent'
-import { BrowserRouter as Router, Route, Routes   } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch   } from 'react-router-dom';
 import Footer from './Components/Footer'
 import Header from './Components/Header'
 import GetAuthor from './Components/GetAuthor'
@@ -12,14 +12,14 @@ function App() {
     <div>
        <Header/>
        <div>
-         <Routes >
-           <Route  path = '/author'>
+         <Switch >
+           <Route exact path = '/author'>
            <GetAuthor/>
            </Route>
            {/* <Route path = '/favauthor'>
            <GetFavAuthor/>
            </Route> */}
-         </Routes >
+         </Switch >
        </div>
        <Footer/>
     </div>
