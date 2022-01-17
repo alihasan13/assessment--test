d6f9866354c52203ccd85e0913783fea2aaf9ffe
Ali Hasan <alihasan13@users.noreply.github.com>
2022-01-17 23:48:24 +0600

    changes made in app.js

--- src/App.js
+++ src/App.js
@@ -1,33 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
-import ListItemComponent from './Components/ListItemComponent'
-import { BrowserRouter as Router, Route, Switch   } from 'react-router-dom';
-import Footer from './Components/Footer'
-import Header from './Components/Header'
-import GetAuthor from './Components/GetAuthor'
-import Drawer from './Components/Drawer'
-import GetFavAuthor from './Components/GetFavAuthor'
+import logo from "./logo.svg";
+import "./App.css";
+import ListItemComponent from "./Components/ListItemComponent";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Footer from "./Components/Footer";
+import Header from "./Components/Header";
+import GetAuthor from "./Container/AllAuthor/GetAuthor";
+import Drawer from "./Components/Drawer";
+import GetFavAuthor from "./Container/AllAuthor/GetFavAuthor";
 
 function App() {
-
   return (
     <Router>
-    <div>
-       <Header/>
-       {/* <GetAuthor/> */}
-       <Drawer />
-       <div>
-         <Switch >
-          
-           {/* <Route path = '/favauthor'>
-           <GetFavAuthor/>
-           </Route> */}
-           <Route exact from="/" render={props => <GetAuthor {...props} />} />
-           <Route exact path="/contact" render={props => <GetFavAuthor {...props} />} />
-         </Switch >
-       </div>
-       <Footer/>
-    </div>
+      <div>
+        <Drawer />
+        </div>
+        <div >
+          <Header />
+          <Switch>
+            <Route
+              exact
+              from="/"
+              render={(props) => <GetAuthor {...props} />}
+            />
+            <Route
+              exact
+              path="/contact"
+              render={(props) => <GetFavAuthor {...props} />}
+            />
+          </Switch>
+        </div>
+        <div>
+        <Footer />
+      </div>
     </Router>
   );
 }
